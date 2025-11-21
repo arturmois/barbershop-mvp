ff8132267931bc0bd57c753e7c09ea75f26e62a3
Artur Moises <arturmoiscontato@gmail.com>
2025-11-20 22:10:49 -0300

    feat: enhance Home component to display recommended and popular barbershops; add BarbershopItem component and Prisma integration for data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,24 @@ import Header from "./_components/header";
 import SearchInput from "./_components/search-input";
 import banner from "@/public/banner.png";
 import BookingItem from "./_components/booking-item";
+import prisma from "@/lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
 
-const Home = () => {
+const Home = async () => {
+  const recommendedBarbershops = await prisma.barbershop.findMany({
+    orderBy: {
+      created_at: "desc",
+    },
+  });
+  const popularBarbershops = await prisma.barbershop.findMany({
+    orderBy: {
+      created_at: "asc",
+    },
+  });
   return (
-    <>
+    <main>
       <Header />
-      <div className="space-y-5 px-5">
+      <div className="space-y-5 p-5">
         <SearchInput />
         <Image
           src={banner}
@@ -25,8 +37,24 @@ const Home = () => {
           barberShopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
           date={new Date("2025-01-01")}
         />
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Recomendadas
+        </h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {recommendedBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
-    </>
+    </main>
   );
 };
 
