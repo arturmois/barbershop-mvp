69017b1191fc98ee20f1c013b97432a173374c9e
Artur Moises <arturmoiscontato@gmail.com>
2025-11-20 22:14:37 -0300

    feat: add Footer component to Home page and adjust spacing in main content

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import banner from "@/public/banner.png";
 import BookingItem from "./_components/booking-item";
 import prisma from "@/lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import Footer from "./_components/footer";
 
 const Home = async () => {
   const recommendedBarbershops = await prisma.barbershop.findMany({
@@ -20,7 +21,7 @@ const Home = async () => {
   return (
     <main>
       <Header />
-      <div className="space-y-5 p-5">
+      <div className="space-y-4 p-5">
         <SearchInput />
         <Image
           src={banner}
@@ -54,6 +55,7 @@ const Home = async () => {
           ))}
         </div>
       </div>
+      <Footer />
     </main>
   );
 };
